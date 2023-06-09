c6dbbcb927261cbb5305d4f5a393ff8f63db85f8
Jake Wehder <jw@email.comjake>
2023-06-09 18:31:48 -0400

    creating a modal that will in the future show both the searchForm and map

--- client/src/features/listing/components/SearchForm.js
+++ client/src/features/listing/components/SearchForm.js
@@ -6,9 +6,19 @@ import usePlacesAutocomplete, {
 import useOnclickOutside from "react-cool-onclickoutside";
 import TextField from '@mui/material/TextField';
 import Popover from '@mui/material/Popover';
-
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
+import Box from '@mui/material/Box';
+import Grow from "@mui/material/Grow";
 
 function SearchForm() {
+    const [selected, setSelected] = useState(null);
+
     const {
         ready,
         value,
@@ -24,29 +34,55 @@ function SearchForm() {
         clearSuggestions();
     });
 
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions();
+    
+        const results = await getGeocode({ address });
+        const { lat, lng } = await getLatLng(results[0]);
+        setSelected({ lat, lng });
+    };
+
     return(
-        <div ref={ref}>
+        <Box ref={ref} onSelect={handleSelect} sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             <TextField
             id="standard-search"
             label="Search field"
             type="search"
             variant="standard"
+            disabled={!ready}
             value={value}
             onChange={(e) => setValue(e.target.value)}
             />
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-      </Popover>
-        </div>
+            <Popover
+                id='simple-popover'
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+                }}
+            >
+            <List>
+                {status ==="OK" && 
+                data.map(({ place_id, description }) => {
+                return (
+                    <>
+                    <Grow>
+                    <ListItem key={place_id} disablePadding>
+                        <ListItemButton>
+                        <ListItemIcon>
+                            <AddLocationIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={description} />
+                        </ListItemButton>
+                    </ListItem>   
+                    <Divider/>
+                    </Grow>
+                    </>
+                )           
+                })}
+            </List>
+            </Popover>
+        </Box>
     )
 }
 
